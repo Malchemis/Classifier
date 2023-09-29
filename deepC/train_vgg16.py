328f505a46723a51ab604c57259f75bb457f9fda
Huimin Zhang <misshuimin@gmail.com>
2023-09-29 18:00:20 +0200

    .

--- deepC/train_vgg16.py
+++ deepC/train_vgg16.py
@@ -100,10 +100,10 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=config['training']['lr'])
     writer = SummaryWriter()
 
-    train(model, optimizer, train_dataloader, writer, epochs=config['training']['epochs'])
+    #train(model, optimizer, train_dataloader, writer, epochs=config['training']['epochs'])
 
     # Load the best model
     model.load_state_dict(torch.load('best_model.pt'))
 
-    test_acc = test(vgg16, test_dataloader)
+    test_acc = test(model, test_dataloader)
     print(f'Test accuracy:{test_acc}')
