4b5f45cf98dbb88a007e55ff990f9f7e8dc1a672
Huimin Zhang <misshuimin@gmail.com>
2023-10-04 14:46:23 +0200

    .

--- deepC/train_vgg.py
+++ deepC/train_vgg.py
@@ -30,12 +30,9 @@ def train(config, model, optimizer, train_dataloader, val_dataloader, writer, sa
         train_data = tqdm(train_dataloader)
         for features, labels in train_data:
             features, labels = features.to(device), labels.to(device)
-            print('labels', labels)
             # Forward pass
             outputs = model(features)
-            print('outputs', outputs)
             _, preds = torch.max(outputs, 1)
-            print('preds', preds)
             # Statistics to compute accuracy and loss
             batch_train_acc = train_acc(preds, labels)
             loss = criterion(outputs, labels)
